bc075e3111d9187fe6483f209eed1213381f0056
Zakhar-Khmurych <zakhar.khmurych@gmail.com>
2023-03-23 22:54:54 +0200

    started dictionary

--- term3lab3v1/Program.cs
+++ term3lab3v1/Program.cs
@@ -135,11 +135,29 @@ namespace term3lab3v1
     public class StringsDictionary
     {
         public const int InitialSize = 10;
-
+        
         private LinkedList[] _buckets = new LinkedList[InitialSize];
 
+        private int loaded;
+        
         public void Add(string key, string value)
         {
+            int hashCode = Math.Abs(key.GetHashCode());
+            int index = hashCode / InitialSize;
+            var add_value = new KeyValuePair(key, value);
+            var _first = _buckets[index];
+            if (_first == null)
+            {
+                _buckets[index] = new LinkedList();
+                _buckets[index].Add(add_value);
+                loaded++;
+            }
+            else
+            {
+                _buckets[index].Add(add_value);
+                loaded++;
+            }
+            
             
         }
 
@@ -159,13 +177,6 @@ namespace term3lab3v1
             return hash;
         }
     }
-    
-    
-    
-    
-    
-    
-    
     
     
     
